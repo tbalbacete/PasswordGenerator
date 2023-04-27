42110b1cdee2d81442318cc63de74e82ffbe6059
Tony Albacete <tbalbacete@gmail.com>
2023-04-26 21:08:27 -0400

    initial commit

---
+++ src/Pages/Footer.styles.tsx
@@ -0,0 +1,15 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const StyledFooter = styled(Box)({
+  display: "flex",
+  backgroundColor: "rgb(9, 54, 63)",
+  padding: "12px 0px",
+  color: "lightgray",
+  alignItems: "center",
+  justifyContent: "center",
+  textAlign: "center",
+  width: "100%",
+  bottom: "0",
+  gap: "5px",
+});
